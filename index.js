f3ccec663115816d3daa60cb45639ac025bce653
aaryan1122 <102250938+aaryan1122@users.noreply.github.com>
2022-12-03 15:59:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+for (let i = 0; i < document.querySelectorAll("button").length; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", function () {
+        var inner=this.innerHTML
+        sound(inner)
+    })
+}
+document.addEventListener("keydown",function(e){
+    sound(e.key)
+})
+function sound(key) {
+    switch (key) {
+        case "w":
+            let t1 = new Audio("sounds/tom-1.mp3")
+            t1.play()
+            break;
+        case "a":
+            let t2 = new Audio("sounds/tom-2.mp3")
+            t2.play()
+            break;
+        case "s":
+            let t3 = new Audio("sounds/tom-3.mp3")
+            t3.play()
+            break;
+        case "d":
+            let t4 = new Audio("sounds/tom-4.mp3")
+            t4.play()
+            break;
+        case "j":
+            let cr = new Audio("sounds/crash.mp3")
+            cr.play()
+            break;
+        case "k":
+            let k = new Audio("sounds/kick-bass.mp3")
+            k.play()
+            break;
+        case "l":
+            let sn = new Audio("sounds/snare.mp3")
+            sn.play()
+            break;
+        default:
+            break;
+}}
